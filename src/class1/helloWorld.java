81f668f7aa6a13ce6c2abf81fad77c4728293da3
khfoysal <khandakerfoysal518@gmail.com>
2025-01-11 21:16:39 +0600

    Submit Java Assignment 01

--- src/class1/helloWorld.java
+++
@@ -1,7 +0,0 @@
-package class1;
-
-public class helloWorld {
-    public static void main(String[] args) {
-        System.out.println("This is first project");
-    }
-}
---
+++ src/class1/identifierTest.java
@@ -0,0 +1,18 @@
+package class1;
+
+public class identifierTest {
+    int mark=50;
+    String name = "Foysal";
+    public void printSomething () {
+        System.out.println("Some thing ");
+
+    }
+
+    public static void main(String[] args) {
+        int mark_50 = 50;
+        String fullName;
+
+
+    }
+
+}
